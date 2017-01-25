74929d6c37bbc9a629011519359b9215421920b8
shankui <shankui@shankuideMacBook-Pro.local>
2017-01-25 16:59:20 +0800

    dev test

--- src/main/java/AdbTool.java
+++ src/main/java/AdbTool.java
@@ -1,5 +1,89 @@
-/**
- * Created by shankui on 17/1/25.
- */
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AdbTool {
+
+    //dev分支测试
+
+    public static void main(String[] args) {
+        String aaa = getDeviceId(1);
+        System.out.println("设备id是: "+getDeviceId(1));
+        //System.out.println(aaa);
+        System.out.println("设备版本号是: "+getAndroidVersion(1));
+    }
+
+
+    public static Process cmd(String command) throws IOException {
+        //System.out.println("调用cmd");
+        return Runtime.getRuntime().exec(command);
+    }
+
+    /**
+     *
+     * @param index 第几个设备,从1
+     * @return 设备id
+     */
+    public static String getDeviceId(int index){
+
+        Process process= null;
+        try {
+            process = Runtime.getRuntime().exec("adb devices");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader isr=new InputStreamReader(process.getInputStream());
+
+        Scanner sc=new Scanner(isr);
+        ArrayList<String> dinfo = new ArrayList<String>();
+        while(sc.hasNext()){
+            //System.out.println(sc.next());
+            dinfo.add(sc.next());
+        }
+        int i = index*2+2;
+        String deviceid = null;
+        try {
+            deviceid = dinfo.get(i);
+        } catch (java.lang.IndexOutOfBoundsException e) {
+            System.out.println("no way,non so much, so take the 1st");
+            deviceid = dinfo.get(4);
+
+        }
+        //System.out.println(deviceid);
+        //System.out.println(dinfo);
+        return deviceid;
+    }
+
+    public static String getAndroidVersion(int index){
+        Process process= null;
+        String cmd = "adb -s "+getDeviceId(index)+" shell getprop ro.build.version.release";
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader isr=new InputStreamReader(process.getInputStream());
+
+        Scanner sc=new Scanner(isr);
+        String version=null;
+        //ArrayList<String> dinfo = new ArrayList<String>();
+        while(sc.hasNext()){
+            //System.out.println(sc.next());
+            version = sc.next();
+        }
+
+        return version;
+
+    }
 }
